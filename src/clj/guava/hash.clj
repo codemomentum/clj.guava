3fb9b3bff64332aab60e6e2c9271518c9af966f9
James Xu <xumingming64398966@gmail.com>
2012-08-02 00:21:06 +0800

    implemented clj.guava.hash

---
+++ src/clj/guava/hash.clj
@@ -0,0 +1,114 @@
+(ns ^{:doc "Clojure wrapper of com.google.common.hash"
+      :author "xumingming"}
+  clj.guava.hash
+  (:use [clj.guava.base :only [check-arg]])
+  (:import [com.google.common.hash HashFunction Hashing Hasher BloomFilter
+            Funnel PrimitiveSink]))
+
+(def ^{:added "0.1" :doc "All the hash algorithm supported."}
+  ALGORITHMS {:md5 (Hashing/md5)
+              :murmur3-32 (Hashing/murmur3-32)
+              :murmur3-128 (Hashing/murmur3-128)
+              :sha1 (Hashing/sha1)
+              :sha256 (Hashing/sha256)
+              :sha512 (Hashing/sha512)
+              :good-fast-hash (Hashing/goodFastHash 32)})
+
+(def ^:dynamic ^{:added "0.1" :doc "seed for :murmur3_128 and :murmur3_32"} *sed* 0)
+(def ^:dynamic ^{:added "0.1" :doc "minimumBits for :good-fast-hash"} *min-bits* 32)
+
+(defn hash
+  "Generates a hash number according to the algorithm specified and the input data provided.
+
+   Usage:
+     (hash :md5  :james 25)    => 2424085208407
+     (hash :sha256 :james 25) => 834506830458608356
+
+   The following hash algorithm are supported:
+     - :md5
+     - :murmur3-32
+     - :murmur3-128
+     - :sha1
+     - :sha256
+     - :sha512
+     - :good-fast-hash
+
+  For :murmur3-32 and :murmur3-128, you can pass a dynamic var: *sed* to change the default sed(default 0).
+  For :good-fast-hash you can pass in a *min-bits* to specify the minimum bits of hash(default 32)"
+  {:added "0.1" :tag long}
+  [algorithm & xs]
+  (check-arg (contains? ALGORITHMS algorithm) (str "Algorithm " algorithm " not supported!"))
+  (check-arg (not-empty xs) "No data supported to the hash.")
+  (let [^HashFunction hf (cond
+                          (and (= :good-fast-hash algorithm)
+                               (thread-bound? #'*min-bits*)) (Hashing/goodFastHash *min-bits*)
+                          (and (contains? #{:murmur3-32 :murmur3-128} algorithm)
+                               (thread-bound? #'*sed*)) (if (= :murmur3-128 algorithm)
+                                                          (Hashing/murmur3-128 *sed*)
+                                                          (Hashing/murmur3-32 *sed*))
+                          :else (ALGORITHMS algorithm))
+        ^Hasher hasher (.newHasher hf)]
+    (doseq [x xs]
+      (cond
+       (instance? Byte x) (.putByte hasher x)
+       (instance? Short x) (.putShort hasher x)
+       (instance? Integer x) (.putInt hasher x)
+       (instance? Long x) (.putLong hasher x)
+       (instance? Float x) (.putFloat hasher x)
+       (instance? Double x) (.putDouble hasher x)
+       (instance? Boolean x) (.putBoolean hasher x)
+       (char? x) (.putChar hasher x)
+       (string? x) (.putString hasher x)
+       (keyword? x) (.putString hasher (str x))
+       :else (.putObject hasher x)))
+    (-> hasher .hash Hashing/padToLong)))
+
+(defn bloom-filter 
+  "Creates a bloom filter with the expected insertion number and false positive probability.
+
+  The data which are expected to be put into includes usual map and record."
+  {:added "0.1" :tag BloomFilter}
+  [expected-insertions false-positive-probability]
+  (let [^Funnel funnel (reify Funnel
+                         (^void funnel [this from ^PrimitiveSink into]
+                           (doseq [value (vals from)]
+                             (cond
+                              (string? value) (.putString into value)
+                              (char? value) (.putChar into value)
+                              (instance? Boolean value) (.putBoolean into value)
+                              (instance? Double value) (.putDouble into value)
+                              (instance? Float value) (.putFloat into value)
+                              (instance? Long value) (.putLong into value)
+                              (instance? Integer value) (.putInt into value)
+                              (instance? Short value) (.putShort into value)
+                              (instance? Byte value) (.putByte into value)))))]
+    (BloomFilter/create funnel expected-insertions false-positive-probability)))
+
+(defn bloom-filter-put!
+  "Puts data into bloom filter"
+  {:added "0.1"}
+  [^BloomFilter bloom-filter x]
+  (.put bloom-filter x))
+
+(defn bloom-filter-contains?
+  "Checks whether a specified value is in the bloom-filter, if
+   the return is false, then the value is absolutely NOT in the filter.
+   but if the return value is true, it means MAYBE in the filter. For the
+   posibility of the 'MAYBE', try the false-positive-probability function."
+  {:added "0.1" :tag boolean}
+  [^BloomFilter bloom-filter x]
+  (.mightContain bloom-filter x))
+
+
+(defn consistent-hash
+  "Assigns to input a 'bucket' in the range [0, buckets), in a uniform
+   manner that minimizes the need for remapping as buckets grows. That is,
+   consistent-hash(h, n) equals:
+     : n - 1, with approximate probability 1/n
+     : consistent-hash(h, n - 1), otherwise (probability 1 - 1/n)
+
+   See the <a href='http://en.wikipedia.org/wiki/Consistent_hashing'>wikipedia
+   article on consistent hashing</a> for more information."
+   {:added "0.1" :tag Integer}
+   [^long hash ^Integer buckets]
+  (Hashing/consistentHash hash buckets))
---
+++ test/clj/guava/test/hash.clj
@@ -0,0 +1,78 @@
+(ns clj.guava.test.hash
+  (:require [clj.guava.hash :as h])
+  (:use [clojure.test])
+  (:import [com.google.common.hash Hashing Hasher HashCode HashFunction]))
+
+(defn test-hash-helper [algo]
+  (let [^HashFunction hf (h/ALGORITHMS algo)
+        ^Hasher hasher (.newHasher hf)]
+    (.putString hasher "james")
+    (.putLong hasher 25)
+    (.putDouble hasher 2.5)
+    (.putChar hasher \a)
+    (.putString hasher ":object")
+    (is (= (-> hasher .hash Hashing/padToLong) (h/hash algo "james" 25 2.5 \a :object)))))
+
+(deftest test-hash
+  (test-hash-helper :md5)
+  (test-hash-helper :murmur3-32)
+  (test-hash-helper :murmur3-128)
+  (test-hash-helper :sha1)
+  (test-hash-helper :sha256)
+  (test-hash-helper :sha512)
+  (test-hash-helper :good-fast-hash))
+
+(deftest test-murmur3-32-with-sed
+  (let [^HashFunction hf (Hashing/murmur3_32 100)
+        ^Hasher hasher (.newHasher hf)]
+    (.putString hasher "james")
+    (.putLong hasher 25)
+    (.putDouble hasher 2.5)
+    (.putChar hasher \a)
+    (.putString hasher ":object")
+    (binding [h/*sed* 100]
+      (is (= (-> hasher .hash Hashing/padToLong) (h/hash :murmur3-32 "james" 25 2.5 \a :object))))))
+
+(deftest test-murmur3-128-with-sed
+  (let [^HashFunction hf (Hashing/murmur3_128 100)
+        ^Hasher hasher (.newHasher hf)]
+    (.putString hasher "james")
+    (.putLong hasher 25)
+    (.putDouble hasher 2.5)
+    (.putChar hasher \a)
+    (.putString hasher ":object")
+    (binding [h/*sed* 100]
+      (is (= (-> hasher .hash Hashing/padToLong) (h/hash :murmur3-128 "james" 25 2.5 \a :object))))))
+
+(deftest test-good-fast-hash-with-min-bits
+  (let [^HashFunction hf (Hashing/goodFastHash 35)
+        ^Hasher hasher (.newHasher hf)]
+    (.putString hasher "james")
+    (.putLong hasher 25)
+    (.putDouble hasher 2.5)
+    (.putChar hasher \a)
+    (.putString hasher ":object")
+    (binding [h/*min-bits* 35]
+      (is (= (-> hasher .hash Hashing/padToLong) (h/hash :good-fast-hash "james" 25 2.5 \a :object))))))
+
+(deftest test-hash-invalid-algorithm
+  (is (thrown? IllegalArgumentException (h/hash :no-such-algorithm "james" 25))))
+
+(deftest test-hash-empty-input
+  (is (thrown? IllegalArgumentException (h/hash :md5))))
+
+(defrecord Person [name age])
+
+(deftest test-bloom-filter
+  (let [bf (h/bloom-filter 100 0.01)
+        rose (Person. "rose" 30)]
+    (h/bloom-filter-put! bf {:name "james" :age 25})
+    (h/bloom-filter-put! bf {:name "jack" :age 30})
+    (h/bloom-filter-put! bf rose)
+    (is (h/bloom-filter-contains? bf {:name "james" :age 25}))
+    (is (h/bloom-filter-contains? bf {:name "jack" :age 30}))
+    (is (h/bloom-filter-contains? bf rose))
+    (is (= false (h/bloom-filter-contains? bf {:name "jamie" :age 25})))))
+
+(deftest test-consistent-hash
+  (is (= (Hashing/consistentHash 100 10) (h/consistent-hash 100 10))))
